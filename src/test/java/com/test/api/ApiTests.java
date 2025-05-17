714446cc38bf1d44df1d5dd72aaefe40cef1cdff
Niliay <nilaybasol23@gmail.com>
2025-05-17 22:36:28 +0300

    initial commit - added Rest Assured API test cases

---
+++ src/test/java/com/test/api/ApiTests.java
@@ -0,0 +1,70 @@
+package com.test.api;
+
+
+import io.restassured.response.Response;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+import static io.restassured.RestAssured.*;
+import static io.restassured.matcher.RestAssuredMatchers.*;
+
+
+public class ApiTests {
+	//----------------Test1: GET
+	@Test
+    public void testGetUser() {
+        Response response =
+            given()                          // -> isteğe özel ayarlar yapılabilir (burada boş)
+            .when()
+            .get("https://jsonplaceholder.typicode.com/users/1") // → API çağrısı yapılır
+            .then()
+            .extract().response();          // -> gelen yanıtı saklıyoruz
+
+        // Status code kontrolü
+        assertEquals(200, response.statusCode());
+
+        // JSON içinden username alanını çekip kontrol etme
+        String username = response.jsonPath().getString("username");
+        assertEquals("Bret", username);
+
+        // Cevap süresi kontrolü
+        assertTrue(response.getTime() < 1000); // ->1000 milisaniyeden kısa sürede geldi mi????
+    }
+
+//-----------------TEST2: POST ile APIye veri gönderme 
+@Test
+public void testCreatePost() {
+    // Göndereceğimiz JSON verisi
+    String requestBody = "{\n" +
+            "  \"title\": \"foo\",\n" +
+            "  \"body\": \"bar\",\n" +
+            "  \"userId\": 1\n" +
+            "}";
+
+    Response response =
+        given()
+            .header("Content-type", "application/json") // JSON olduğunu belirtiyoruz
+            .and()
+            .body(requestBody)                         // Göndereceğimiz veri
+        .when()
+            .post("https://jsonplaceholder.typicode.com/posts") // POST isteği
+        .then()
+            .extract().response();
+
+    // Status code 201 (Created) olmalı
+    assertEquals(201, response.statusCode());
+
+    // Cevap içeriğinde gönderdiğimiz veriler var mı kontrol edelim
+    String title = response.jsonPath().getString("title");
+    String body = response.jsonPath().getString("body");
+    int userId = response.jsonPath().getInt("userId");
+
+    assertEquals("foo", title);
+    assertEquals("bar", body);
+    assertEquals(1, userId);
+
+    // Performans: 1 saniyeden kısa sürede dönmeli
+    assertTrue(response.getTime() < 1000);
+} 
+
+}
